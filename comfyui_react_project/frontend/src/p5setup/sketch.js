b50e9260642de8caa3e427a50baf0205405ee76e
Koston Speakman <xcoolcat502@gmail.com>
2024-11-24 04:43:47 -0500

    Add files via upload

---
+++ comfyui_react_project/frontend/src/p5setup/sketch.js
@@ -0,0 +1,31 @@
+export default function sketch(p) {
+  let theShader;
+  let img;
+
+  p.preload = () => {
+    theShader = p.loadShader(
+      '/src/shaders/vertex.glsl',
+      '/src/shaders/fragment.glsl'
+    );
+  };
+
+  p.setup = () => {
+    p.createCanvas(600, 600, p.WEBGL);
+    p.noStroke();
+  };
+
+  p.draw = () => {
+    if (img) {
+      p.shader(theShader);
+      theShader.setUniform('u_resolution', [p.width, p.height]);
+      theShader.setUniform('u_time', p.millis() / 1000.0);
+      theShader.setUniform('u_texture', img);
+      p.rect(0, 0, p.width, p.height);
+    }
+  };
+
+  p.updateImage = (newImage) => {
+    img = newImage;
+    p.resizeCanvas(img.width, img.height);
+  };
+}
