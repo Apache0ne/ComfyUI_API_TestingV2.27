babcb9c82a5a46c99242ceb8ab30f105dca46668
Koston Speakman <xcoolcat502@gmail.com>
2024-11-27 08:43:30 -0500

    Add files via upload

---
+++ ADTEST/frontend/src/components/SideMenu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ModelSelector from './ModelSelector';
+import '../styles/SideMenu.css';
+
+const SideMenu = ({ isSideMenuOpen, toggleSideMenu, onModelSelect }) => {
+  return (
+    <div className={`side-menu ${isSideMenuOpen ? 'open' : 'closed'}`}>
+      <button className={`fold-button ${isSideMenuOpen ? 'open' : ''}`} onClick={toggleSideMenu}>
+        {isSideMenuOpen ? '«' : '»'}
+      </button>
+      {isSideMenuOpen && (
+        <div className="side-menu-content">
+          <h3>Select Model and LoRA</h3>
+          <ModelSelector onModelSelect={onModelSelect} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SideMenu;
